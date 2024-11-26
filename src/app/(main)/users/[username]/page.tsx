77e2be5fe70a5d847c4e4e8a6c909ad47936dc02
elgafr <elgaputrifr@gmail.com>
2024-11-26 22:29:24 +0700

    fix profile

--- src/app/(main)/users/[username]/page.tsx
+++ src/app/(main)/users/[username]/page.tsx
@@ -1,11 +1,17 @@
 import { validateRequest } from "@/auth";
+import FollowButton from "@/components/FollowButton";
+import FollowerCount from "@/components/FollowerCount";
 import TrendsSidebar from "@/components/TrendsSidebar";
+import { Button } from "@/components/ui/button";
 import UserAvatar from "@/components/UserAvatar";
 import prisma from "@/lib/prisma";
 import { FollowerInfo, getUserDataSelect, UserData } from "@/lib/types";
+import { formatNumber } from "@/lib/utils";
+import { formatDate } from "date-fns";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { cache } from "react";
+import UserPosts from "./UserPosts";
 
 interface PageProps {
   params: { username: string };
@@ -58,6 +64,13 @@ export default async function Page({ params: { username } }: PageProps) {
     <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <UserProfile user={user} loggedInUserId={loggedInUser.id} />
+        <div className="rounded-2xl bg-card p-5 shadow-sm">
+          <h2 className="text-center text-2xl font-bold">
+            {user.displayName}&apos;s posts
+          </h2>
+
+        </div>
+        <UserPosts userId={user.id} />
       </div>
       <TrendsSidebar />
     </main>
@@ -84,6 +97,38 @@ async function UserProfile({ user, loggedInUserId }: UserProfileProps) {
         size={250}
         className="mx-auto size-full max-h-60 max-w-60 rounded-full"
       />
+      <div className="flex flex-wrap gap-3 sm:flex-nowrap">
+        <div className="me-auto space-y-3">
+          <div>
+            <h1 className="text-3xl font-bold">{user.displayName}</h1>
+            <div className="text-muted-foreground">@{user.username}</div>
+
+          </div>
+          <div>Member since {formatDate(user.createdAt, "MMM d, yyyy")}</div>
+          <div className="flex items-center gap-3">
+            <span>
+              Posts:{" "}
+              <span className="font-semibold">
+                {formatNumber(user._count.posts)}
+              </span>
+            </span>
+            <FollowerCount userId={user.id} initialState={followerInfo} />
+          </div>
+        </div>
+        {user.id === loggedInUserId ? (
+          <Button>Edit profile</Button>
+        ) : (
+          <FollowButton userId={user.id} initialState={followerInfo} />
+        )}
+      </div>
+      {user.bio && (
+        <>
+        <hr />
+        <div className="overflow-hidden whitespace-pre-line break-words">
+          {user.bio}
+        </div>
+        </>
+      )}
     </div>
   );
 }
--- src/components/posts/editor/mutations.ts
+++ src/components/posts/editor/mutations.ts
@@ -2,15 +2,26 @@ import { useToast } from "@/hooks/use-toast"
 import { submitPost } from "./actions"
 import { InfiniteData, QueryFilters, useMutation, useQueryClient } from "@tanstack/react-query"
 import { PostsPage } from "@/lib/types"
+import { useSession } from "@/app/(main)/SessionProvider"
 
 export function useSubmitPostMutation() {
     const { toast } = useToast()
     const queryClient = useQueryClient()
 
+    const { user } = useSession()
+
     const mutation = useMutation({
         mutationFn: submitPost,
         onSuccess: async (newPost) => {
-            const queryFilter: QueryFilters = { queryKey: ["post-feed", "for-you"] }
+            const queryFilter = {
+                queryKey: ["post-feed"], predicate(query) {
+                    return (
+                        query.queryKey.includes("for-you") ||
+                        (query.queryKey.includes("user-posts") && query.queryKey.includes(user.id))
+
+                    )
+                }
+            } satisfies QueryFilters
             await queryClient.cancelQueries(queryFilter)
 
             queryClient.setQueriesData<InfiniteData<PostsPage, string | null>>(
@@ -38,7 +49,7 @@ export function useSubmitPostMutation() {
             queryClient.invalidateQueries({
                 queryKey: queryFilter.queryKey,
                 predicate(query) {
-                    return !query.state.data
+                    return queryFilter.predicate(query) && !query.state.data
                 }
             })
 
@@ -49,10 +60,10 @@ export function useSubmitPostMutation() {
         },
         onError(error) {
             console.error(error);
-                toast({
-                    variant: "destructive",
-                    description: "Failed to post. Please try again."
-                })
+            toast({
+                variant: "destructive",
+                description: "Failed to post. Please try again."
+            })
         },
 
     })
