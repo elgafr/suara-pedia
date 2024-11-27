86a4b18a578d5f664e219ed0a5eb42193e0def37
elgafr <elgaputrifr@gmail.com>
2024-11-27 23:21:33 +0700

    update user tooltip

--- src/app/api/users/username/[username]/route.ts
+++ src/app/api/users/username/[username]/route.ts
@@ -1,4 +1,6 @@
 import { validateRequest } from "@/auth";
+import prisma from "@/lib/prisma";
+import { getUserDataSelect } from "@/lib/types";
 
 export async function GET(req: Request, { params: { username } }: { params: { username: string } }
 ) {
@@ -9,6 +11,22 @@ export async function GET(req: Request, { params: { username } }: { params: { us
         if(!loggedInUser) {
             return Response.json({ error: "Unauthorized" }, { status: 401 });
         }
+
+        const user = await prisma.user.findFirst({
+            where: {
+                username: {
+                    equals: username,
+                    mode: "insensitive"
+                }
+            },
+            select: getUserDataSelect(loggedInUser.id)
+
+        })
+
+        if(!user) {
+            return Response.json({ error: "User not found" }, { status: 404 });
+        }
+        return Response.json(user)
     } catch (error) {
         console.error(error);
         return Response.json({ error: "Internal server error" }, { status: 500 });
--- src/components/Linkify.tsx
+++ src/components/Linkify.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { LinkIt, LinkItUrl } from "react-linkify-it";
+import UserLinkWithTooltip from "./UserLinkWithTooltip";
 
 interface LinkifyProps {
   children: React.ReactNode;
@@ -26,13 +27,12 @@ function LinkifyUsername({ children }: LinkifyProps) {
     <LinkIt
       regex={/@[a-zA-Z0-9_]{3,16}/}
       component={(match, key) => (
-        <Link
-          key={key}
-          href={`/users/${match.slice(1)}`}
-          className="text-primary hover:underline"
-        >
+
+        <UserLinkWithTooltip key={key} username={match.slice(1)}>
           {match}
-        </Link>
+        </UserLinkWithTooltip>
+          
+        
       )}
     >
       {children}
